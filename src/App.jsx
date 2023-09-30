586245eb53a3044a54b802647309b9ef18be2742
Oscar Hilton <oscarhilton@hotmail.com>
2023-09-30 12:08:19 +0100

    tailwind styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from 'react';
+import { useFormik } from 'formik';
+import { useDropzone } from 'react-dropzone';
+import Logo from "./assets/logo.png"
 
-function App() {
-  // State variable to hold file list
-  const [files, setFiles] = useState([]);
+const MyUploadComponent = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      description: '',
+      date_of_air: ''
+    },
+    onSubmit: async (values) => {
+      const formData = new FormData();
+      formData.append('file', acceptedFiles[0]);
+      formData.append('name', values.name);
+      formData.append('description', values.description);
+      formData.append('date_of_air', values.date_of_air);
 
-  // https://flask-production-be46.up.railway.app/upload
-  useEffect(() => {
-    // Function to fetch files from Flask API
-    const fetchFiles = async () => {
-      try {
-        const response = await fetch('https://flask-production-be46.up.railway.app/list_files');
-        const data = await response.json();
-        if (data.files) {
-          setFiles(data.files);
-        }
-      } catch (error) {
-        console.error("Error fetching files:", error);
-      }
-    };
+      // Make your API call here
+      console.log(values);
+    }
+  });
 
-    // Call the fetch function
-    fetchFiles();
-  }, []);
+  const { getRootProps, getInputProps, acceptedFiles } = useDropzone();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <form onSubmit={formik.handleSubmit} className="p-8 bg-gray-100 rounded-lg shadow-md animate__animated animate__fadeIn">
+      <div {...getRootProps()} className="p-4 border-4 border-dashed border-gray-200 rounded-lg cursor-pointer hover:bg-gray-300 transition duration-300">
+        <input {...getInputProps()} />
+        <p className="text-center">Drag 'n' drop some files here, or click to select files</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>Choo Choo! This is an example of a Vite + React app running on Railway.</p>
-        <h2>List of Files</h2>
-        <ul>
-          {files.map((file, index) => (
-            <li key={index}>{file}</li>
-          ))}
-        </ul>
+      <div className="mt-4">
+        <input
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+          className="p-2 w-full rounded-md border border-gray-300"
+          placeholder="Name"
+        />
       </div>
-    </>
+      <div className="mt-4">
+        <input
+          name="description"
+          onChange={formik.handleChange}
+          value={formik.values.description}
+          className="p-2 w-full rounded-md border border-gray-300"
+          placeholder="Description"
+        />
+      </div>
+      <div className="mt-4">
+        <input
+          name="date_of_air"
+          type="date"
+          onChange={formik.handleChange}
+          value={formik.values.date_of_air}
+          className="p-2 w-full rounded-md border border-gray-300"
+        />
+      </div>
+      <div className="mt-4">
+        <button type="submit" className="p-2 w-full bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300">
+          Submit
+        </button>
+      </div>
+    </form>
   );
-}
+};
 
-export default App;
+export default () => (
+  <main>
+    <div className='fixed bottom-10 right-10 w-20 h-20'>
+    <img src={Logo} />
+    </div>
+  <div className="flex justify-center items-center h-screen w-screen">
+    <MyUploadComponent />
+  </div>
+  </main>
+  );
