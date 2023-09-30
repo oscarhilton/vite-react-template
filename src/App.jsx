a98de1ebe77728f289a520c580e3d820d316c4bf
Oscar Hilton <oscarhilton@hotmail.com>
2023-09-30 13:07:45 +0100

    bells and whistles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useFormik } from 'formik';
 import { useDropzone } from 'react-dropzone';
 import Logo from "./assets/logo.png"
+import image1 from "./assets/1.jpg"
+import image2 from "./assets/2.jpg"
+import image3 from "./assets/3.jpg"
 
 const MyUploadComponent = () => {
   const formik = useFormik({
@@ -66,13 +69,54 @@ const MyUploadComponent = () => {
   );
 };
 
-export default () => (
-  <main>
-    <div className='fixed bottom-10 right-10 w-20 h-20'>
-    <img src={Logo} />
+const ImageGallery = () => {
+  // Replace these with the names of your downloaded images
+  const imageNames = [image1, image2, image3];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % imageNames.length);
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(timer);
+  }, [imageNames]);
+
+  return (
+    <div className="relative w-full h-80 w-80 overflow-hidden rounded-lg m-2">
+      {imageNames.map((imageName, index) => (
+        <img
+          key={imageName}
+          src={`${imageName}`}
+          alt={`Carousel image ${index + 1}`}
+          className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000 ease-in-out ${currentIndex === index ? 'opacity-100' : 'opacity-0'}`}
+        />
+      ))}
     </div>
-  <div className="flex justify-center items-center h-screen w-screen">
-    <MyUploadComponent />
+  );
+};
+
+export default () => (
+  <main className='m-10'>
+  <div className="sm:m-auto block sm:flex sm:justify-center sm:items-center sm:h-screen sm:w-screen">
+    <div className='w-full sm:w-auto'>
+      <h1 className='mb-2'>AudioFormatPro.</h1>
+      <p className='mb-8'>
+        Format all your audio files in one place to provide to your internal team.
+      </p>
+      <MyUploadComponent />
+    </div>
+    <div className='m-8 hidden sm:block w-full sm:w-auto'>
+      <div className='mt-28'>
+        <ImageGallery />
+        <span className='font-mono text-xs text-center w-full sm:w-auto block'
+          >all images sourced from <a href="https://www.facebook.com/resonancefm/?locale=en_GB">facebook.</a>
+        </span>
+      </div>
+    </div>
   </div>
+    <footer className='sm:fixed mt-10 bottom-10 right-10 flex font-mono text-sm items-center text-gray-500'>
+    Made with ❤️ for <img className="mx-2" width={30} height={18} src={Logo} /> <span className='mx-2'>by</span> <a href="#">Object2</a>
+    </footer>
   </main>
   );
